f1c8db88745b0fa4d9ec6c0f0b8e4e029ccc044f
Brandon Taylor <brandon.taylor221@gmail.com>
2018-01-29 00:07:30 -0500

    tada

--- src/OnlinePackage.jl
+++ src/OnlinePackage.jl
@@ -1,5 +1,17 @@
 module OnlinePackage
 
-# Package code goes here.
+import HTTP
+import JSON
+
+abstract type Remote end
+
+include("talk_to.jl")
+include("github.jl")
+include("travis.jl")
+include("travis_token.jl")
+include("appveyor.jl")
+include("ssh_keygen.jl")
+include("configure.jl")
+include("generate.jl")
 
 end
