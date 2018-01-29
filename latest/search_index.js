fe2cd60635ad03a3c8c2e11d7022e9f997e4c94a
autodocs <autodocs>
2018-01-29 05:42:14 +0000

    build based on 84a4d86

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,11 +9,27 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#OnlinePackage.generate-Tuple{Any}",
+    "page": "Home",
+    "title": "OnlinePackage.generate",
+    "category": "Method",
+    "text": "generate(repo_name, create_appveyor = true)\n\nGenerate GitHub, Travis, and (optionally) AppVeyor remotes for a repository. BE SURE TO ADD .jl AT THE END OF THE REPO_NAME FOR JULIA REPOSITORIES.\n\n\n\n"
+},
+
+{
+    "location": "index.html#OnlinePackage.set_up-Tuple{Any,Any,Any}",
+    "page": "Home",
+    "title": "OnlinePackage.set_up",
+    "category": "Method",
+    "text": "set_up(username, github_token, appveyor_token; ssh_keygen_file = \"ssh-keygen\")\n\nSet up OnlinePackage.\n\nGet a github_token here. Make sure to check the \"public_repo\" and \"delete_repo\" scopes.\n\nGet a appveyor_token here.\n\nThe default ssh-keygen_file assumes ssh-keygen is in your path. For Windows users with git installed, try ssh_keygen_file = \"C:/Program Files/Git/usr/bin/ssh-keygen\".\n\n\n\n"
+},
+
+{
     "location": "index.html#OnlinePackage-1",
     "page": "Home",
     "title": "OnlinePackage",
     "category": "section",
-    "text": "(Image: Stable) (Image: Latest) (Image: Build Status) (Image: CodeCov)"
+    "text": "Modules = [OnlinePackage]"
 },
 
 ]}
