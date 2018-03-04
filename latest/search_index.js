1807ff638e8f7af4a0aeabd77fede759140e4360
autodocs <autodocs>
2018-03-04 16:13:41 +0000

    build based on 9a3cceb

--- latest/search_index.js
+++ latest/search_index.js
@@ -12,7 +12,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#OnlinePackage.generate-Tuple{Any}",
     "page": "Home",
     "title": "OnlinePackage.generate",
-    "category": "Method",
+    "category": "method",
     "text": "generate(repo_name, create_appveyor = true)\n\nGenerate GitHub, Travis, and (optionally) AppVeyor remotes for a repository.\n\n\n\n"
 },
 
@@ -20,7 +20,7 @@ var documenterSearchIndex = {"docs": [
     "location": "index.html#OnlinePackage.set_up-Tuple{Any,Any,Any}",
     "page": "Home",
     "title": "OnlinePackage.set_up",
-    "category": "Method",
+    "category": "method",
     "text": "set_up(username, github_token, appveyor_token; ssh_keygen_file = \"ssh-keygen\")\n\nSet up OnlinePackage.\n\nGet a github_token here. Make sure to check the \"public_repo\" and \"delete_repo\" scopes.\n\nGet a appveyor_token here.\n\nThe default ssh-keygen_file assumes ssh-keygen is in your path. For Windows users with git installed, try ssh_keygen_file = \"C:/Program Files/Git/usr/bin/ssh-keygen\".\n\n\n\n"
 },
 
