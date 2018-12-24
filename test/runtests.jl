f602906c6c83bbe3e224b5cf02540db96e847287
Brandon Taylor <brandon.taylor221@gmail.com>
2018-12-23 21:48:35 -0500

    fixing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,16 @@
 using Documenter: makedocs, deploydocs
 import OnlinePackage
 
+root = joinpath(dirname(@__DIR__), "docs"),
+
 makedocs(
     modules = [OnlinePackage],
     sitename = "OnlinePackage.jl",
-    root = joinpath(dirname(@__DIR__), "docs"),
+    root = root,
     strict = true
 )
 
 deploydocs(
+    root = root,
     repo = "github.com/bramtayl/OnlinePackage.jl.git",
 )
